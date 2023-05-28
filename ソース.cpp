71e41ae11452297acae3009d6d4a63f6596da0e2
Satori32 <Huga8989+githubf@gmail.com>
2023-05-28 11:51:35 +0900

    add TimeTimer.

--- ソース.cpp
+++ ソース.cpp
@@ -1,5 +1,44 @@
 #include <iostream>
 #include <cstdint>
+#include <chrono>
+#include <thread>
+#include <future>
+
+class StopWatch {
+	std::chrono::high_resolution_clock::time_point S;
+	std::chrono::high_resolution_clock::time_point E;
+public:
+
+	typedef std::chrono::milliseconds TimeType;
+
+	StopWatch() { Start(); };
+
+	bool Start() {
+		S = E = std::chrono::high_resolution_clock::now();
+		return true;
+	}
+	bool ReStart() {
+		return Start();
+	}
+	bool Stop() {
+		E = std::chrono::high_resolution_clock::now();
+		return true;
+	}
+
+	bool Reset() {
+		S = E = std::chrono::high_resolution_clock::now();
+		return true;
+	}
+	template<class T = TimeType>
+	T Ellipse() {
+		return std::chrono::duration_cast<T>(std::chrono::high_resolution_clock::now() - S);
+	}
+	template<class T = TimeType>
+	T Result() {
+		return std::chrono::duration_cast<T>(E - S);
+	}
+
+};
 
 template<class INT>
 class CycleTimer {
@@ -43,6 +82,64 @@ protected:
 	INT M = 7;
 };
 
+template<class INT>
+class TimeTimer :public CycleTimer<INT> {
+public:
+	TimeTimer(const INT& Mod, std::chrono::nanoseconds NS) :CycleTimer<INT>(Mod), Wait(NS) {}
+
+
+	bool Start() {
+		auto Fun = [this]() { while (IsRunning_) { this->Update(); std::this_thread::sleep_for(Wait); }};
+		IsRunning_ = true;
+		Fu = std::async(std::launch::async, Fun);
+
+		return true;
+	}
+	bool Stop() {
+		IsRunning_ = false;
+		return true;
+	}
+
+	virtual ~TimeTimer() {
+		Stop();
+	}
+
+	bool IsRunning() {
+		return IsRunning_;
+	}
+protected:
+	template<class T>
+	T MakeCopy(T In) { return In; }
+
+protected:
+	bool IsRunning_ = false;
+	std::chrono::nanoseconds Wait = { 0 };
+	std::future<void> Fu;
+};
+
+int main() {
+
+	TimeTimer<std::uintmax_t> TT(7, std::chrono::seconds(2));
+
+	TT.Start();
+
+	while (TT.IsRunning()){
+		std::cout << TT.Count() << std::endl;
+
+		if (TT.IsTail()) {
+			std::cout << "to end thread." << std::endl;
+			TT.Stop();
+		}
+	}
+
+	std::cout << "end the app." << std::endl;
+
+	std::this_thread::sleep_for(std::chrono::seconds(3));
+	
+	return 0;
+
+}
+/** /
 int main() {
 	CycleTimer<int> CT(16);
 
@@ -55,3 +152,4 @@ int main() {
 
 	return 0;
 }
+/**/
